e2eb064803e577496f518b38ffd18880dea3454d
Rodrigo Gonçalves <rodrigo.ag.de@gmail.com>
2023-05-15 15:28:51 -0300

    Vercel

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
-const cors = require('cors')
+//const cors = require('cors')
 const express = require('express')
 const router = require('./router')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+//app.use(cors())
 app.use('/api', router)
 
 app.listen(process.env.PORT || 3333, () => {
