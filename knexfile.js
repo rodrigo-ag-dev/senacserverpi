748afb241c2f74a865fc4b9391eb1977f6737ee0
Rodrigo Gonçalves <rodrigo.ag.de@gmail.com>
2023-05-15 15:07:03 -0300

    Vercel

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,22 @@
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './src/database/db.sqlite' },
+    client: 'pg',
+    connection: process.env.DB_CONNECTION,
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   },
 
   staging: {
-    client: 'sqlite3',
-    connection: { filename: './src/database/db.sqlite' },
+    client: 'pg',
+    connection: process.env.DB_CONNECTION,
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   },
 
   production: {
-    client: 'sqlite3',
-    connection: { filename: './src/database/db.sqlite' },
+    client: 'pg',
+    connection: process.env.DB_CONNECTION || 'postgres://rodrigo-ag-dev:kmTL5EiHWX9x@ep-frosty-bird-907998.us-east-2.aws.neon.tech/senacpi?ssl=true',
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   }
-
 };
--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 const cors = require('cors')
 const express = require('express')
-const routes = require('./routes')
+const router = require('./router')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use('/api', routes)
+app.use('/api', router)
 
 app.listen(process.env.PORT || 3333, () => {
     console.log('Servidor rodando na porta ', process.env.PORT || 3333, '!')
