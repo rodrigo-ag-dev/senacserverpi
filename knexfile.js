6d8ed45f99935aa4dac9958d95707fb6d5b21a06
Rodrigo Gonçalves <rodrigo.ag.de@gmail.com>
2023-05-16 18:01:39 -0300

    remove .env

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,23 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    client: process.env.DB_CLIENT || 'pg',
-    connection: process.env.DB_CONNECTION || 'postgres://rodrigo-ag-dev:kmTL5EiHWX9x@ep-frosty-bird-907998.us-east-2.aws.neon.tech/senacpi?ssl=true',
+    client: process.env.NODE_DB_CLIENT,
+    connection: process.env.NODE_DB_CONNECTION,
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   },
 
   staging: {
-    client: process.env.DB_CLIENT || 'pg',
-    connection: process.env.DB_CONNECTION,
+    client: process.env.NODE_DB_CLIENT,
+    connection: process.env.NODE_DB_CONNECTION,
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   },
 
   production: {
-    client: process.env.DB_CLIENT || 'pg',
-    connection: process.env.DB_CONNECTION,
+    client: process.env.NODE_DB_CLIENT,
+    connection: process.env.NODE_DB_CONNECTION,
     migrations: { directory: './src/database/migrations' },
     useNullAsDefault: true,
   }
